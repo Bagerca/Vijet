2db6dd950b62a223cc9e6937a362bdd7bd8402ee
Bagerca <bagrationbebia@gmail.com>
2026-05-27 19:38:40 +0300

    3

--- js/games-db.js
+++ js/games-db.js
@@ -89,7 +89,7 @@ window.GamesDatabase = {
         title: "S.T.A.L.K.E.R.: Тень Чернобыля", 
         cover: "img/covers/stalker.png", 
         rating: "9.0 / 10", 
-        year: 2077, 
+        year: 2007, 
         genre: "Шутер / Выживание", 
         developer: "GSC Game World", 
         platform: "PC" 
