6a924ceb0aa5bbdc3023a30582768418a84e8ab4
Bagerca <bagrationbebia@gmail.com>
2026-07-08 17:09:15 +0300

    1

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 /* ФАЙЛ: js/config.js */
 /* ================= ОСНОВНОЙ КОНФИГ СИСТЕМЫ ================= */
 window.AppConfig = {
-    channelName: "bagercaa",
+    channelName: "ksusha__sher",
     botChannel: "bagercaa",
     allowedUsers: ["bagercaa", "to_be_ang", "treebals"], 
     
